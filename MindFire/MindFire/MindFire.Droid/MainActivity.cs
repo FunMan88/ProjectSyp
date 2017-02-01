40f74560007784417d5000ae7a1b7b1ba8bdd025
Marko Diljkan <marko.diljkan@yahoo.com>
2017-02-01 09:46:41 +0100

    Client-Connection wurde implementiert

--- MindFire/MindFire/MindFire.Droid/MainActivity.cs
+++ MindFire/MindFire/MindFire.Droid/MainActivity.cs
@@ -6,28 +6,49 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using com.shephertz.app42.gaming.multiplayer.client;
+using com.shephertz.app42.gaming.multiplayer.client.listener;
+using com.shephertz.app42.gaming.multiplayer.client.events;
 
 namespace MindFire.Droid
 {
 	[Activity (Label = "MindFire.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
+	public class MainActivity : Activity, ConnectionRequestListener
 	{
-		int count = 1;
+		private WarpClient w;
+
+		public void onConnectDone(ConnectEvent eventObj)
+		{
+			StartActivity(typeof(MenuActitvity));
+			this.Finish();
+		}
+
+		public void onDisconnectDone(ConnectEvent eventObj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void onInitUDPDone(byte resultCode)
+		{
+			throw new NotImplementedException();
+		}
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
-			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
+			WarpClient.initialize(Constants.apiKey, Constants.secretKey);
+			w = WarpClient.GetInstance();
+			w.AddConnectionRequestListener(this);
+
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+			EditText et = FindViewById<EditText> (Resource.Id.editText1);
+
 			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+				w.Connect(et.Text);
 			};
+
 		}
 	}
 }
