f24540c88fa69acaf37ee5dcfe7e7c86042bf107
Marko Diljkan <marko.diljkan@yahoo.com>
2017-02-16 10:23:45 +0100

    Fragen können erstellt werden

--- MindFire/MindFire/MindFire.Droid/MainActivity.cs
+++ MindFire/MindFire/MindFire.Droid/MainActivity.cs
@@ -1,31 +1,38 @@
 ﻿using System;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 using com.shephertz.app42.gaming.multiplayer.client;
 using com.shephertz.app42.gaming.multiplayer.client.listener;
 using com.shephertz.app42.gaming.multiplayer.client.events;
+using com.shephertz.app42.gaming.multiplayer.client.command;
+using com.shephertz.app42.paas.sdk.csharp;
+using Android.Support.V7.App;
 
 namespace MindFire.Droid
 {
 	[Activity (Label = "MindFire.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity, ConnectionRequestListener
+	public class MainActivity : AppCompatActivity, ConnectionRequestListener
 	{
 		private WarpClient w;
 
 		public void onConnectDone(ConnectEvent eventObj)
 		{
-			StartActivity(typeof(MenuActitvity));
-			this.Finish();
+			if (eventObj.getResult() == WarpResponseResultCode.SUCCESS)
+			{
+				StartActivity(typeof(MenuActitvity));
+				this.Finish();
+			}
+			else
+			{ 
+				RunOnUiThread(() => Toast.MakeText(this, "Connection error " + eventObj.getResult(), ToastLength.Long).Show());
+			}
 		}
 
 		public void onDisconnectDone(ConnectEvent eventObj)
 		{
-			throw new NotImplementedException();
+			
 		}
 
 		public void onInitUDPDone(byte resultCode)
@@ -33,23 +40,27 @@ namespace MindFire.Droid
 			throw new NotImplementedException();
 		}
 
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate (bundle);
-			SetContentView (Resource.Layout.Main);
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
 
 			WarpClient.initialize(Constants.apiKey, Constants.secretKey);
+			App42API.Initialize(Constants.apiKey, Constants.secretKey);
+
 			w = WarpClient.GetInstance();
 			w.AddConnectionRequestListener(this);
 
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			EditText et = FindViewById<EditText> (Resource.Id.editText1);
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			EditText et = FindViewById<EditText>(Resource.Id.editText1);
 
-			button.Click += delegate {
+			button.Click += delegate
+			{
 				w.Connect(et.Text);
 			};
 
 		}
+
 	}
 }
 
